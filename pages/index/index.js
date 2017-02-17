40ab9e32e993211a17560c17ec92e0987cb0bba1
code4a <jiangyantao8@gmail.com>
2017-02-17 18:10:25 +0800

    add index page weather

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,16 @@ Page({
     this.audioCtx = wx.createAudioContext('myAudio')
   },
   data: {
+
+    AK: 'hWtYHdhw2fIwi9PWsWX09IVmGtLVp2UG',
+    city: '',
+    temp: '',
+    todayDayImg: '',
+    todayNightImg: '',
+    weather: '',
+    todayDate: '',
+    todayTime: '',
+
     playTime: 0,
     playing: false,
     curPos: '00:00',
@@ -63,8 +73,16 @@ Page({
       duration: util.formatAudioTime(u.detail.duration)
     });
   },
+  // 监听用户下拉动作
+  onPullDownRefresh: function () {
+      this.getTime();
+  },
   onLoad: function () {
     console.log('onLoad')
+
+    this.loadInfo();
+    this.getTime();
+
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
@@ -79,5 +97,79 @@ Page({
       title: '欢迎页面分享',
       path: '/pages/index/index'
     }
-  }
+  },
+  loadInfo: function () {
+        var that = this;
+        wx.getLocation({
+            type: 'gcj02',
+            success: function(res) {
+                var latitude = res.latitude;
+                var longitude = res.longitude;
+                var AK = that.data.AK;
+               that.loadCity(latitude, longitude, AK, that.loadWeather);
+               console.log(that.data.city);
+            //    that.loadWeather(that.data.city, AK);
+            }
+        })
+    },
+    loadCity: function (latitude, longitude, AK, callback) {
+        var that = this;
+        var url = 'https://api.map.baidu.com/geocoder/v2/?location=' + latitude + ',' + longitude + '&output=json&ak=' + AK;
+        wx.request({
+            url: url,
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res);
+                var city = res.data.result.addressComponent.city;
+                that.setData({city :city});
+                console.log(that.data.city);
+                callback && callback(city, AK);
+            }
+        })
+    },
+    loadWeather: function (city, AK) {
+        var that  = this;
+        var url = 'https://api.map.baidu.com/telematics/v3/weather?location=' + city + '&output=json&ak=' + AK;
+        console.log(url);
+        wx.request({
+            url: url,
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                var desNum = that.random(5);
+                console.log(res);
+                var future = res.data.results[0].weather_data.filter(function(ele, index) {
+                    return index > 0;
+                });
+                var temReg = /\d+℃/;
+                that.setData({
+                    temp: res.data.results[0].weather_data[0].date.match(temReg)[0],
+                    todayDayImg: res.data.results[0].weather_data[0].dayPictureUrl,
+                    todayNightImg: res.data.results[0].weather_data[0].nightPictureUrl,
+                    weather: res.data.results[0].weather_data[0].weather + ' | ' + res.data.results[0].weather_data[0].wind,
+                });
+            console.log(that.data.future);
+
+            }
+        })
+    },
+    random: function(max, min) {
+        var min = min || 0;
+        return Math.floor(Math.random() * (max - min +1) + min);
+    },
+    getTime: function () {
+        var date = new Date();
+        var minute = date.getMinutes() >=10 ? date.getMinutes() : ('0' + date.getMinutes());
+        var hour = date.getHours() >=10 ? date.getHours() : ('0' + date.getHours());
+        var now = date.getDate() >= 10 ? date.getDate() : ('0' + date.getDate());
+        var todayDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + now;
+        var todayTime = hour + ':' + minute;
+        this.setData({
+            todayDate: todayDate,
+            todayTime: todayTime
+        })
+    }
 })
